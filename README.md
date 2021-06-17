5aae3d005d3f84e98155766cb135efaaed80ea4a
Matheus Ferreira <matheus.ferreira9@hotmail.com>
2021-06-17 19:56:04 -0300

    Projeto 100% - Buscador de letras e músicas

---
+++ README.md
@@ -0,0 +1,32 @@
+# O Projeto
+Projeto para pesquisar letras de músicas/bandas através da API do Deezer e Vagalume, com adicional de mostrar prévia das músicas, nome e imagem do álbum.
+
+A criação desse projeto foi para aprimorar e por em prática o meu conhecimento na linguagem JavaScript.
+
+<img src="images/project.png">
+
+<a href="https://iammatheus.github.io/search-lyrics-two-api">Visualizar Projeto</a>
+
+<br>
+
+## Porque utilizar duas API?
+Inicialmente o projeto estava sendo desenvolvido com a API 
+<a href="https://lyricsovh.docs.apiary.io">Lyrics Ovh</a> e nela eu obtia todas as informações que eu precisava, mas ela está em manutenção, então eu migrei o projeto para a API do 
+<a href="https://developers.deezer.com/api">Deezer</a>, que não fornece as letras e utilizei a API do <a href="https://api.vagalume.com.br/docs">Vagalume</a> para obter elas.
+
+API Deezer utilizada para obter: 
+  - Nome do artista
+  - Nome da música 
+  - Nome e imagem do álbum
+  - Prévia da música
+
+API Vagalume utilizada para obter:
+  - Letra das músicas
+
+<br>
+
+## Desenvolvimento
+  - Javascript
+  - HTML
+  - CSS3
+  - Visual Studio Code
---
+++ script.js
@@ -0,0 +1,252 @@
+const form = document.querySelector("#form");
+const sectionContainer = document.querySelector("section");
+const prevAndNextContainer = document.querySelector(".prevAndNextContainer");
+const searchInput = document.querySelector("#typeHere")
+const duration = 30
+
+const apiUrl = "https://api.deezer.com/search?q=";
+const corsUrl = "https://api.codetabs.com/v1/proxy?quest=";
+
+const clearPrevAndNextContainer = () => {
+  prevAndNextContainer.innerHTML = "";
+};
+
+const clearSearchInput = () => {
+  searchInput.value = "";
+};
+
+const warningMessage = (message) => {
+  sectionContainer.innerHTML = `<li class="warning-message">${message}</li>`;
+};
+
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  return await response.json();
+};
+
+const verifyPrevAndNextButtons = (prev, next) => {
+  prevAndNextContainer.innerHTML = `
+    ${prev ? `<p href="#top" id="prevClick" class="btnPrevNext" onClick="getMoreMusic('${prev}')">Anteriores</p>` : ""}
+    ${next ? `<p href="#top" id="nextClick" class="btnPrevNext" onClick="getMoreMusic('${next}')">Próximas</p>` : ""}
+  `;
+  const prevClick = document.querySelector('#prevClick')
+  const nextClick = document.querySelector('#nextClick')
+
+  if(prevClick !== null){
+    prevClick.addEventListener('click', e => {
+      scrollTop(e.target)
+    })
+  }
+  if(nextClick !== null){
+    nextClick.addEventListener('click', e => {
+      scrollTop(e.target)
+    })
+  }
+};
+
+const insertMusicIntoPage = async ({ data, prev, next }) => {
+  clearSearchInput();
+
+  sectionContainer.innerHTML = await data.map((
+    { artist:{ name }, album:{ cover_medium:img_album }, album:{title:title_album}, title, preview }) => 
+  `<div class="artist-container">  
+    <aside>
+
+      <header>
+        <div class="album-container">
+          <img src="${img_album}" alt="${title_album}" class="album">
+          <p>${title_album}</p>
+        </div>
+          <h1>${title}</h1>
+          <p>${name}</p>  
+      </header>
+
+      <div class="previews-music" id="previews-music">
+          <div class="bg-time" id="bg-time">
+            <div class="fill-time" id="fill-time"></div>
+          </div>
+          <div class="play-pause">
+            <i class="fas fa-play" id="fas" data-audio="${preview}"></i>
+          </div> 
+          <p>Preview</p>
+      </div>
+
+      <audio id="audio" src="${preview}"></audio>
+        
+      </aside>
+
+      <button
+        href="#top"
+        class="btn view-lyrics" 
+        data-artist="${name.replaceAll("/"," ")}" 
+        data-title-music="${title.replaceAll(".","")}">
+           Ver letra
+      </button>
+
+    </div>`).join("");
+
+  /* Código "for" necessário para que ao setar o tempo da música o 
+     progresso da barra e a música não voltem para o início */
+  var fasClass = document.querySelectorAll('#fas')
+  var progress = document.querySelectorAll("#fill-time")
+
+  for(let i = 0; i < 15; i++){
+    fasClass[i].classList.add(i)
+    progress[i].classList.add(i)
+  }
+
+  if (prev || next) {
+    verifyPrevAndNextButtons(prev, next);
+    return;
+  }
+  clearPrevAndNextContainer();
+
+};
+
+const searchMusic = async (music) => {
+  const data = await fetchData(`${corsUrl}${apiUrl}${music}`);
+  insertMusicIntoPage(data);
+};
+
+const getMoreMusic = async (music, e) => {
+  const data = await fetchData(`${corsUrl}${music}`);
+  insertMusicIntoPage(data);
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const searchTerm = searchInput.value.trim().replaceAll("/"," ")
+
+  if(!searchTerm) {
+    warningMessage(
+      "Por favor, preencha o campo acima ou insira um termo válido!"
+    );
+    clearPrevAndNextContainer();
+    clearSearchInput()
+    return;
+  }
+  searchMusic(searchTerm);
+});
+
+const searchLyrics = async (artist, titleMusic) => {
+  const apiLyrics = `https://api.vagalume.com.br/search.php?art=`
+  const key = `3a1a2a27471b147a3a4d4f7650c76ad8`
+  const data = await fetchData(`${apiLyrics}${artist}&mus=${titleMusic}&apikey=${key}`);
+  
+  const lyrics = data.mus[0].text.replace(/(\r\n|\r|\n)/g, "<br>");
+  const artista = artist.replace("'", "");
+  
+  sectionContainer.innerHTML = `
+    <div class="lyrics-container">
+       <h2>
+          ${titleMusic} - ${artist}
+       </h2>
+       <p class="lyrics">${lyrics}</p>
+       <button class="btn btn-back" onClick="searchMusic('${artista}')">Voltar</button>
+       <a href="https://www.vagalume.com.br" target="_blank"><img src="images/vagalume.png" class="logo-vagalume"></img></a>
+    </div>
+  `;
+};
+
+async function getAndShowLyrics(e){
+  const clickedElement = e.target;
+
+  if (clickedElement.tagName === "BUTTON") {
+    const artist = clickedElement.getAttribute("data-artist");
+    const titleMusic = clickedElement.getAttribute("data-title-music");
+
+    await searchLyrics(artist, titleMusic);
+    scrollTop(clickedElement)
+    clearPrevAndNextContainer();
+  }
+}
+sectionContainer.addEventListener("click", getAndShowLyrics);
+
+function updateProgress(e){
+  const {currentTime} = e.srcElement
+  const fas = document.querySelectorAll("#fas")
+  const audio = document.querySelectorAll("#audio")
+  const fillProgress = document.querySelectorAll("#fill-time")
+
+  const progressPercent = (currentTime / duration) * 100
+
+  for(let i = 0; i < audio.length; i++) {
+    audio[i].classList.add(i)
+    if(e.target.classList.contains(i)){
+      fillProgress[i].style.width = `${progressPercent}%`
+    }
+    if(audio[i].ended && progressPercent >= 99.5){
+      fas[i].classList.add('fa-play')
+      fas[i].classList.remove('fa-pause')
+    }
+  }
+}
+
+function setProgress(e) {
+  const audio = document.querySelectorAll("#audio")
+  const progressContainer = document.querySelectorAll('#bg-time')
+  
+  const width = this.clientWidth
+  const clickX = e.offsetX
+  const currentTime = (clickX / width) * duration
+
+  for(let i = 0; i < 15; i++){
+    progressContainer[i].classList.add(i)
+    if(e.target.classList.contains(i)){
+      audio[i].currentTime = currentTime
+    }
+  }
+}
+
+const playPauseMusic = async e => {
+  const clickedElement = e.target;
+  const music = clickedElement.getAttribute("data-audio")
+  const fas = document.querySelectorAll("#fas")
+  const audio = document.querySelectorAll("#audio")
+  const progressContainer = document.querySelectorAll('#bg-time')
+  
+  if(clickedElement.classList.contains("fas")) {
+    for(let o = 0; o < fas.length; o++) {
+      audio[o].pause()
+
+      if(clickedElement.classList.contains("fa-pause") && audio[o].src == music){
+        for(let i = 0; i < fas.length; i++){
+          fas[i].classList.add('fa-play')
+          fas[i].classList.remove('fa-pause')
+        }
+        audio[o].src = audio[o].src 
+
+        await audio[o].pause()
+        
+      }else if(clickedElement.classList.contains("fa-play") && audio[o].src == music){
+        for(let i = 0; i < fas.length; i++){
+          fas[i].classList.add('fa-play')
+          fas[i].classList.remove('fa-pause')
+        }
+        audio[o].src = audio[o].src 
+
+        clickedElement.classList.add('fa-pause')
+        clickedElement.classList.remove('fa-play')
+
+        audio[o].addEventListener('play', e => {
+          audioPlay = e.target
+          audioPlay.addEventListener('timeupdate', updateProgress)
+        })
+        progressContainer[o].addEventListener('click', setProgress)
+        await audio[o].play()
+      }
+    }
+  }
+}
+sectionContainer.addEventListener("click", playPauseMusic);
+
+function scrollTop(e){
+  const id = e.getAttribute('href')
+  const to = document.querySelector(id).offsetTop
+
+  window.scroll({
+     top: to,
+     behavior: "smooth"
+  })
+}
